5a84ea2b962e5374b1097eea96138924b4e07e55
Людок <skobeleva.ludmila@yandex.ru>
2022-11-30 16:35:54 +0300

    часть курсовой

--- src/Employee.java
+++ src/Employee.java
@@ -1,19 +1,45 @@
 public class Employee {
     private static int idGenerator=1;
 
-    private int id;
-    private String fio;
+    private final int id;
+    private final String fio;
     private int salary;
     private int department;
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFio() {
+        return fio;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
 
     public Employee (String fio, int salary, int department){
         this.id=idGenerator ++;
         this.fio=fio;
-        this.department=department;
-        this.id=id;
+        this.department= department;
+        this.salary=salary;
+
+    }
+    @Override
+    public String toString(){
+        return "id: " + id+ " ФИО: " +fio+ " Отдел: " + department+ " ЗП "+salary;
     }
 
-    private int year = 2018;
-    private static Employee[] employeesArr = new Employee[20];
-    private static int index = 0; // 2
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,23 @@
 public class Main {
-    public static void main(String[] args) {
-        String [] fio={}
 
-        System.out.println("Hello world!");
+    private static final Employee[] EMPLOYEES = new Employee[10];
+    public static void main(String[] args) {
+        EMPLOYEES[0] = new Employee("Иванов Николай Иванович", 87_000, 2);
+        EMPLOYEES[1] = new Employee("Петров Александр Иванович", 82_000, 1);
+        EMPLOYEES[2] = new Employee("Аванесян Олег Сергеевич", 77_000, 2);
+        EMPLOYEES[3] = new Employee("Сидов Никита Петрович", 97_000, 3);
+        EMPLOYEES[4] = new Employee("Васин Василий Константинович", 87_000, 1);
+        EMPLOYEES[5] = new Employee("Бунин Николай Николаевич", 95_000, 4);
+        EMPLOYEES[6] = new Employee("Гагарин Илья Николаевич", 99_000, 5);
+        EMPLOYEES[7] = new Employee("Дудин Сергей Сергеевич", 91_000, 4);
+        EMPLOYEES[8] = new Employee("Елин Игорь Николаевич", 93_000, 5);
+        EMPLOYEES[9] = new Employee("Жужин Илья Олегович", 98_000, 3);
+
+        printFullInfo();
+    }
+    private static void printFullInfo(){
+        for (Employee employee: EMPLOYEES){
+            System.out.println(employee);
+        }
     }
 }
